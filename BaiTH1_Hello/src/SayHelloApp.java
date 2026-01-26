0e4121110fcda36b6f617d0315b7f0f56cabb72c
Le Chi Hai <hailehcl1@gmail.com>
2026-01-26 14:18:58 +0700

    cập nhật code
    thử pus

--- BaiTH1_Hello/src/SayHelloApp.java
+++ BaiTH1_Hello/src/SayHelloApp.java
@@ -13,6 +13,7 @@ public class SayHelloApp {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Xin chao, toi la sinh vien NTU va dang hoc lap trinh Java.");
+		System.out.println("Xin chao, thử pust");
 	}
 
 }
